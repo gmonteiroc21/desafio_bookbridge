a642ae263361aa3b7e0c5be82f9d8868ecf48ef4
Gabriel Monteiro <gmcs@cin.ufpe.br>
2024-11-11 02:49:30 -0300

    atualização dos scripts
    correção do local de commit

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -28,3 +28,22 @@ def calculate_user_literary_age(interactions_df, books_df):
     }).reset_index()
 
     return user_literary_age
+
+def user_preference(interactions_df, genres_df):
+    # Realizar o merge entre interações e gêneros para associar cada livro lido ao seu gênero
+    user_genre_interactions = interactions_df.merge(genres_df[['book_id', 'genres']], on='book_id', how='left')
+
+    # Expandir os gêneros para garantir que cada gênero fique em uma linha separada
+    user_genre_interactions = user_genre_interactions.explode('genres')
+
+    # Contar as ocorrências de cada gênero para cada usuário
+    user_genre_count = user_genre_interactions.groupby(['user_id', 'genres']).size().reset_index(name='genre_count')
+    # Ordenar por 'user_id' e 'genre_count' para que o gênero mais lido apareça primeiro para cada usuário
+    top_genre_per_user = user_genre_count.sort_values(['user_id', 'genre_count'], ascending=[True, False])
+
+    # Remover duplicatas para manter apenas o gênero mais lido por cada usuário
+    top_genre_per_user = top_genre_per_user.drop_duplicates(subset='user_id', keep='first')
+
+    # Renomear a coluna 'genres' para 'favorite_genre'
+    top_genre_per_user = top_genre_per_user.rename(columns={'genres': 'favorite_genre'})
+    return top_genre_per_user
