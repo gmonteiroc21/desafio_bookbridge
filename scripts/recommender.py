02e6e25ed98d6694f2420b8f8c22a3c3f31badc8
Gabriel Monteiro <gmcs@cin.ufpe.br>
2024-11-11 11:06:04 -0300

    adicionando função melhor livro pro usuario
    essa função faz uma relação entre o genero preferido do autor e o livro de maior nota desse genero

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -1,24 +1,11 @@
-def recommend_books(user_id, user_ranking_df, genre_stats_df, interactions_df, books_df):
-    # Passo 1: Obter o gênero favorito do usuário
-    favorite_genre = user_ranking_df[user_ranking_df['user_id'] == user_id]['favorite_genre'].values[0]
-    
-    # Passo 2: Obter os melhores livros desse gênero
-    genre_books = genre_stats_df[genre_stats_df['genres'] == favorite_genre]
-    if genre_books.empty:
-        return f'Nenhum livro encontrado para o gênero favorito: {favorite_genre}'
-    
-    best_books = genre_books[['genres', 'best_book_title', 'best_book_average_rating']]
+def recommend_books(user_id, top_genre_per_user, genre_stats_df):
+    user_genre = top_genre_per_user[top_genre_per_user['user_id'] == user_id]['favorite_genre'].iloc[0]
 
-    # Passo 3: Filtrar livros não lidos pelo usuário
-    books_read_by_user = interactions_df[interactions_df['user_id'] == user_id]['book_id']
-    books_to_recommend = books_df[~books_df['book_id'].isin(books_read_by_user)]
-    
-    # Passo 4: Ordenar os livros recomendados por avaliação
-    books_to_recommend = books_to_recommend[books_to_recommend['genres'] == favorite_genre]
-    books_to_recommend = books_to_recommend.sort_values(by='average_rating', ascending=False)
-    
-    # Selecionar os top N livros para recomendação
-    top_books = books_to_recommend.head(10)  # Top 10 livros recomendados
-    
-    return top_books[['title', 'average_rating', 'publication_year']]
+    # Filtra o DataFrame de estatísticas de gêneros para pegar a linha correspondente ao gênero
+    best_book_in_genre = genre_stats_df[genre_stats_df['genres'] == user_genre]
 
+    # Agora, você pode acessar as informações do melhor livro desse gênero
+    best_book = best_book_in_genre['title'].iloc[0]  # Pega o nome do melhor livro
+
+    # Exibe o melhor livro do gênero
+    return best_book
